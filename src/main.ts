57d0269893e81a831f19ab1b4e77fb89f4bb4bd3
Chenyt26 <2396164919@qq.com>
2022-05-17 14:45:45 +0800

    define input
    Match-id-257922afd61c2205dc8e5d9604575a9df1ba2c03

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import * as core from '@actions/core';
+
+import * as context from './context';
 export async function run() {
     core.info("Generate settings.xml for Maven Builds")
+    const inputs: context.Inputs = context.getInputs();
+    for (let key in inputs) {
+        console.log(key)
+        console.log((inputs as any)[key]); 
+        // do something
+      }
 }
   
 run().catch(core.setFailed);
